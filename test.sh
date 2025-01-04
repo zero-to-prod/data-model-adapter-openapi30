393c3e833c525a95c8abeb9f7d35330e30179745
david_smith <david_smith@sweetwater.com>
2025-01-04 14:52:14 -0500

    Update `test.sh`.

--- test.sh
+++ test.sh
@@ -7,7 +7,6 @@ for version in "${php_versions[@]}"; do
   docker compose run --rm "${version}composer" composer update --no-cache
 
   if ! docker compose run --rm "$version" vendor/bin/phpunit; then
-    echo "Tests failed on $version."
     exit 1
   fi
 done
