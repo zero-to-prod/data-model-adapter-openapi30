b5bccdadb68ea754fb3f557f5b9d16141e832f9c
david <dasmith@zero-to-prod.com>
2025-01-20 16:38:35 -0500

    bugfix/const-comment (#5)
    * bugfix/const-comment Updated `src/OpenApi30.php`.
    
    * bugfix/const-comment Created `tests/Acceptance/Constants/CommentWithoutDescription/CommentWithoutDescriptionTest.php`.
    
    * bugfix/const-comment Created `tests/Acceptance/Constants/CommentWithoutDescription/schema.json`.
    
    * bugfix/const-comment Updated `tests/Acceptance/Constants/Comments/CommentsTest.php`.
    
    * bugfix/const-comment Updated `tests/Acceptance/Constants/Comments/schema.json`.
    
    ---------
    
    Co-authored-by: david_smith <david_smith@sweetwater.com>

--- tests/Acceptance/Constants/CommentWithoutDescription/CommentWithoutDescriptionTest.php
+++ tests/Acceptance/Constants/CommentWithoutDescription/CommentWithoutDescriptionTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Acceptance\Constants\Comments;
+namespace Tests\Acceptance\Constants\CommentWithoutDescription;
 
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
@@ -10,13 +10,15 @@ use Zerotoprod\DataModelGenerator\Models\Config;
 use Zerotoprod\DataModelGenerator\Models\ConstantConfig;
 use Zerotoprod\DataModelGenerator\Models\ModelConfig;
 
-class CommentsTest extends TestCase
+class CommentWithoutDescriptionTest extends TestCase
 {
+
     #[Test] public function generate(): void
     {
         Engine::generate(
             OpenApi30::adapt(file_get_contents(__DIR__.'/schema.json')),
             Config::from([
+                Config::namespace => 'App\\DataModels',
                 Config::directory => self::$test_dir,
                 Config::model => [
                     ModelConfig::constants => [
@@ -31,41 +33,10 @@ class CommentsTest extends TestCase
             expectedFile: self::$test_dir.'/User.php',
             actualString: <<<PHP
                 <?php
+                namespace App\DataModels;
                 class User
                 {
-                /**
-                 * description
-                 *
-                 * @see \$name
-                 */
-                public const name = 'name';
-                public string \$name;
-                }
-                PHP
-        );
-    }
-
-    #[Test] public function disable_comment_constant(): void
-    {
-        Engine::generate(
-            OpenApi30::adapt(file_get_contents(__DIR__.'/schema.json')),
-            Config::from([
-                Config::directory => self::$test_dir,
-                Config::model => [
-                    ModelConfig::constants => [
-                        ConstantConfig::comments => false,
-                    ],
-                    ModelConfig::properties => []
-                ]
-            ])
-        );
-
-        self::assertStringEqualsFile(
-            expectedFile: self::$test_dir.'/User.php',
-            actualString: <<<PHP
-                <?php
-                class User
-                {
+                /** @see \$name */
                 public const name = 'name';
                 public string \$name;
                 }
