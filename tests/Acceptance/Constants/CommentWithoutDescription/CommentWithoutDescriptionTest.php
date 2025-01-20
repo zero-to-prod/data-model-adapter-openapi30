0c4a26ec9e0d3352378e66c1c202792486a02317
david_smith <david_smith@sweetwater.com>
2025-01-20 16:37:10 -0500

    bugfix/const-comment Created `tests/Acceptance/Constants/CommentWithoutDescription/CommentWithoutDescriptionTest.php`.

---
+++ tests/Acceptance/Constants/CommentWithoutDescription/CommentWithoutDescriptionTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Acceptance\Constants\CommentWithoutDescription;
+
+use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
+use Zerotoprod\DataModelAdapterOpenapi30\OpenApi30;
+use Zerotoprod\DataModelGenerator\Engine;
+use Zerotoprod\DataModelGenerator\Models\Config;
+use Zerotoprod\DataModelGenerator\Models\ConstantConfig;
+use Zerotoprod\DataModelGenerator\Models\ModelConfig;
+
+class CommentWithoutDescriptionTest extends TestCase
+{
+
+    #[Test] public function generate(): void
+    {
+        Engine::generate(
+            OpenApi30::adapt(file_get_contents(__DIR__.'/schema.json')),
+            Config::from([
+                Config::namespace => 'App\\DataModels',
+                Config::directory => self::$test_dir,
+                Config::model => [
+                    ModelConfig::constants => [
+                        ConstantConfig::comments => true,
+                    ],
+                    ModelConfig::properties => []
+                ]
+            ])
+        );
+
+        self::assertStringEqualsFile(
+            expectedFile: self::$test_dir.'/User.php',
+            actualString: <<<PHP
+                <?php
+                namespace App\DataModels;
+                class User
+                {
+                /** @see \$name */
+                public const name = 'name';
+                public string \$name;
+                }
+                PHP
+        );
+    }
+}
