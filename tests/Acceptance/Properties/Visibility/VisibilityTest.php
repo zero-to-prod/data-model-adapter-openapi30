3affd016c24b4ae769ec23a827783736579769b0
david_smith <david_smith@sweetwater.com>
2025-01-20 14:03:33 -0500

    feat/refactor-to-data-model-engine-v2 Updated `tests/Acceptance/Properties/Visibility/VisibilityTest.php`.

--- tests/Acceptance/Properties/Visibility/VisibilityTest.php
+++ tests/Acceptance/Properties/Visibility/VisibilityTest.php
@@ -7,6 +7,7 @@ use Tests\TestCase;
 use Zerotoprod\DataModelAdapterOpenapi30\OpenApi30;
 use Zerotoprod\DataModelGenerator\Engine;
 use Zerotoprod\DataModelGenerator\Models\Config;
+use Zerotoprod\DataModelGenerator\Models\ModelConfig;
 use Zerotoprod\DataModelGenerator\Models\PropertyConfig;
 use Zerotoprod\DataModelGenerator\Models\Visibility;
 
@@ -15,16 +16,16 @@ class VisibilityTest extends TestCase
     #[Test] public function generate(): void
     {
         Engine::generate(
-            OpenApi30::adapt(
-                file_get_contents(__DIR__.'/openapi30.json'),
-                Config::from([
-                    Config::directory => self::$test_dir,
-                    Config::properties => [
-                        PropertyConfig::visibility => Visibility::protected
-                    ],
-                    Config::exclude_constants => true,
-                ])
-            )
+            OpenApi30::adapt(file_get_contents(__DIR__.'/openapi30.json')),
+            Config::from([
+                Config::directory => self::$test_dir,
+                Config::model => [
+                    ModelConfig::properties => [
+                        PropertyConfig::visibility => Visibility::protected,
+                        PropertyConfig::types => ['number' => 'float']
+                    ]
+                ]
+            ])
         );
 
         self::assertStringEqualsFile(
