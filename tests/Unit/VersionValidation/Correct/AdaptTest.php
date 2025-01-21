9355345c043a4a2e2f6e7e1607cfb8f81ab8cc86
david_smith <david_smith@sweetwater.com>
2025-01-21 13:20:06 -0500

    maint/implement-changes-in-engine Updated `tests/Unit/VersionValidation/Correct/AdaptTest.php`.

--- tests/Unit/VersionValidation/Correct/AdaptTest.php
+++ tests/Unit/VersionValidation/Correct/AdaptTest.php
@@ -6,7 +6,6 @@ use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 use Zerotoprod\DataModelAdapterOpenapi30\OpenApi30;
 use Zerotoprod\DataModelGenerator\Models\Components;
-use Zerotoprod\DataModelGenerator\Models\Config;
 
 class AdaptTest extends TestCase
 {
@@ -16,9 +15,6 @@ class AdaptTest extends TestCase
             is_a(
                 object_or_class: OpenApi30::adapt(
                     file_get_contents(__DIR__.'/schema.json'),
-                    Config::from([
-                        Config::directory => self::$test_dir
-                    ])
                 ),
                 class: Components::class
             )
